9f7672b5768c10577fe42094a122a06a8b6c1af1
GUEYE Mohamed <gueyemo@hostname.iut2.upmf-grenoble.fr>
2018-10-24 11:14:45 +0200

    petite modif

---
+++ modele/data/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Produit (
+      id INTEGER,
+      categorie STRING,
+      type STRING,
+      genre STRING,
+      prix FLOAT,
+      marque STRING,
+      reference STRING,
+      taille STRING,
+      vendeur STRING,
+      disponibilite STRING
+      cover STRING,
+);
