f893f9f252ac5d62efa5f25cda41d281e5052e52
unknown <crosvincent0@gmail.com>
2018-10-27 21:13:02 +0200

    changements mineurs

--- controlleur/ProduitDetailctrl.php
+++ controlleur/ProduitDetailctrl.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../modele/ProduitclassDAO.php');
-var_dump($DAO->nb());
+//var_dump($DAO->nb());
 $id=max(($_GET['id']??1),1);
 $id=min($id,$DAO->nb());
 $Produit = $DAO->get($id);
